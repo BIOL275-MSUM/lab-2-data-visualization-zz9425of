74e5a1809ac30c0fc47c029d0a736335abb301aa
Nicole Nimako-Boateng <zz9425of@mnstate.edu>
2022-01-20 13:28:50 -0600

    added code, now finshed, submit this one lab 2

--- assignment.R
+++ assignment.R
@@ -10,15 +10,20 @@ ggplot(data = penguins) +
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species))
 
+#this created color on the scatter plot
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))
 
+#this created color and shape for species and island on the scatter plot
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))+
 facet_wrap(~species, nrow=1)
 
+#this created in columns
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))+
   facet_grid(island~species)
+
+#this created 3 columns and rows, a grid
 
 
