518046cce98fac8e03dd59fa506a8e93027d5d77
Nicole Nimako-Boateng <zz9425of@mnstate.edu>
2022-01-20 13:24:44 -0600

    add new code

--- assignment.R
+++ assignment.R
@@ -5,6 +5,7 @@ ggplot(data=penguins)
 
 ggplot(data = penguins) + 
   geom_point(mapping = aes(x=flipper_length_mm,y=bill_depth_mm ))
+#this made a scatter plot
 
 ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species))
@@ -13,5 +14,11 @@ ggplot(data=penguins) +
   geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))
 
 ggplot(data=penguins) +
-  geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))
-facet_wrap(~species, nrow=2)
+  geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))+
+facet_wrap(~species, nrow=1)
+
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x=flipper_length_mm,y=bill_depth_mm,color=species,shape=island))+
+  facet_grid(island~species)
+
+
